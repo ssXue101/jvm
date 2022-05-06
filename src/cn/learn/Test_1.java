9cfe0eef7bca7c07ff6b1a92d840c8193c612940
ssXue <shaoshuai.xue@cienet.com.cn>
2022-05-06 05:00:32 -0700

    test github connection status

---
+++ src/cn/learn/Test_1.java
@@ -0,0 +1,10 @@
+package cn.learn;
+
+public class Test_1 {
+
+    public static void main(String[] args) {
+        System.out.println("dan bu tiao shi open jdk");
+        Test_1 test_1 = new Test_1();
+        while (true);
+    }
+}
